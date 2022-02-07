ce23ded2e5c7694fe322c78dcab938095cd3384b
Jakub Pomykała <jakub.pomykala@gmail.com>
2022-02-07 14:34:15 +0100

    Add i18n configuration

---
+++ next-i18next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es', 'pl', 'fr_FR'],
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {appWithTranslation} from 'next-i18next';
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+function MyApp({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
